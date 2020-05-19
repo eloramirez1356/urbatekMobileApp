b944dda4c13d07be33ab58bbc745594cae88fe18
Eloy Ramirez Hernanz <Eloy.RamirezHernanz@earlywarning.com>
2020-05-19 01:46:19 -0500

    Improved functionality of Sign in with React hooks forms

--- src/components/login/LoginView.js
+++ src/components/login/LoginView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import {Text, View, TextInput, Button} from 'react-native';
 import LoginViewStyles from './LoginViewStyles';
 
@@ -19,6 +19,42 @@ const LoginView = ({navigation}) => {
       />
     </View>
   );
+};
+
+export default LoginView;*/
+
+import React, { useEffect } from "react";
+import { Text, View, TextInput, Button, Alert } from "react-native";
+import { useForm } from "react-hook-form";
+import LoginViewStyles from './LoginViewStyles';
+
+const LoginView = ({navigation}) => {
+    const { register, setValue, handleSubmit, errors } = useForm();
+    const onSubmit = data => {Alert.alert("Form Data", JSON.stringify(data));navigation.navigate('HomePage');};
+
+    useEffect(() => {
+        register({ name: 'email'}, { required: true });
+        register({ name: 'password'}, {required: true});
+    }, [register]);
+
+
+    return (
+        <View>
+            <Text>Email</Text>
+            <TextInput style={LoginViewStyles.input}
+                onChangeText={text => setValue("email", text, true)}
+            />
+            {errors.email && <Text>"Your input is required"</Text>}
+
+            <Text>Password</Text>
+            <TextInput style={LoginViewStyles.input}
+                onChangeText={text => setValue("password", text, true)}
+            />
+            {errors.password && <Text>"Your input is required"</Text>}
+
+            <Button title="Login" onPress={handleSubmit(onSubmit)}></Button>
+        </View>
+    );
 };
 
 export default LoginView;
--- src/components/login/LoginViewStyles.js
+++ src/components/login/LoginViewStyles.js
@@ -1,10 +1,22 @@
 import {StyleSheet} from 'react-native';
 
 const LoginViewStyles = StyleSheet.create({
-  input: {
-    borderWidth: 1,
-    borderColor: '#000000',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 10,
+    backgroundColor: '#181e34',
   },
+  formContainer: {
+    padding: 8,
+    flex: 1,
+  },
+  button: {
+    backgroundColor: 'red',
+  },
+  text: {
+    color: '#181e34'
+  }
 });
 
 export default LoginViewStyles;
